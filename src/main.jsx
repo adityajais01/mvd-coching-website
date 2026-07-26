1569ad1ac083ad1b1926a5ed082586ecaac40fa1
adityajais01 <jaiskrishna77@gmail.com>
2026-07-26 15:59:12 -0500

    Fix: Clean extension paths in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider> {/*Auth Context Wrapped */}
-        <App />
+    <AuthProvider>
+      <App />
     </AuthProvider>
   </BrowserRouter>
 )
